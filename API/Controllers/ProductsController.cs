9e94128e40f0df3c9179c1753b6fd579e51119f4
don.jasto <jakov.stosic@gmail.com>
2020-04-28 16:56:59 +0200

    Comments added for Products API

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,12 +6,21 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
+        /// <summary>
+        /// Api for get list of products
+        /// </summary>
+        /// <returns>List of products</returns>
         [HttpGet]
         public string GetProducts()
         {
             return "list of products";
         }
 
+        /// <summary>
+        /// Api for get the product
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Product</returns>
         [HttpGet("{id}")]
         public string GetProduct(int id)
         {
