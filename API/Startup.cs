268136c7ac80401637b99c6303f348e17a78f9bc
don.jasto <jakov.stosic@gmail.com>
2020-05-06 17:25:55 +0200

    Added Swagger for api doc , ignored Error Controller

--- API/Startup.cs
+++ API/Startup.cs
@@ -17,6 +17,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -57,6 +58,11 @@ namespace API
                     return new BadRequestObjectResult(errorResponse);
                 };
             });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Shop API", Version = "v1" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -73,6 +79,12 @@ namespace API
             app.UseStaticFiles();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => 
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shop API v1");
+                });
 
             app.UseEndpoints(endpoints =>
             {
