92d96dfc55cbe7a188471f50c156426183c3039b
don.jasto <jakov.stosic@gmail.com>
2020-05-07 23:25:19 +0200

    Add Http Client return response

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = "Shop";
+export class AppComponent implements OnInit {
+  title = 'Shop';
+  products:any[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
+      (response: any) => {
+        this.products = response.data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
