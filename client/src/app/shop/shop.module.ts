0178956110cd35da9ac0861547c9d8f3e5c73dcc
don.jasto <jakov.stosic@gmail.com>
2020-05-10 21:24:12 +0200

    Created module home with c home and product details

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
-  declarations: [ShopComponent, ProductItemComponent],
+  declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
   imports: [CommonModule, SharedModule],
   exports: [ShopComponent]
 })
