2c0ec44b3007daef07f98b9261a5e883fde3b5f4
don.jasto <jakov.stosic@gmail.com>
2020-05-25 12:37:55 +0200

    Creating additional error components server-error , not-found

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -8,6 +10,8 @@ import { ProductDetailsComponent } from './shop/product-details/product-details.
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'test-error', component: TestErrorComponent },
+  { path: 'server-error', component: ServerErrorComponent },
+  { path: 'not-found', component: NotFoundComponent },
   {
     path: 'shop',
     loadChildren: () =>
