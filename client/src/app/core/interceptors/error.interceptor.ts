a2d71aee399ee6f142c591c90a22cd903420abb1
don.jasto <jakov.stosic@gmail.com>
2020-05-25 13:24:02 +0200

    Creating Http Interceptor for handling server-error and not-found

---
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,37 @@
+import { error } from 'util';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+
+// have to make it Injectable
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private router: Router) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      // tslint:disable-next-line: no-shadowed-variable
+      catchError((error) => {
+        if (error) {
+          if (error.status === 404) {
+            this.router.navigateByUrl('/not-found');
+          }
+          if (error.status === 500) {
+            this.router.navigateByUrl('/server-error');
+          }
+        }
+        return throwError(error);
+      })
+    );
+  }
+}
